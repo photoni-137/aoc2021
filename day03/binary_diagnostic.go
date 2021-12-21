294e7d6d3c3fd7cc2f8d44ce7974c380cecdc031
Anton Eder <anton.eder@tngtech.com>
2021-12-21 20:43:55 +0100

    streamline day03

--- day03/binary_diagnostic.go
+++ day03/binary_diagnostic.go
@@ -1,42 +1,31 @@
 package main
 
 import (
-	"bufio"
+	"aoc2021/shared"
 	"fmt"
-	"io"
-	"log"
-	"os"
 	"strconv"
 	"strings"
 )
 
 type bitSlice []int
 
-func handle(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func bitSliceToInt(bin bitSlice) (number int64, err error) {
+func bitSliceToInt(bin bitSlice) int {
 	var sb strings.Builder
 	for _, bit := range bin {
 		sb.WriteString(strconv.Itoa(bit))
 	}
-	number, err = strconv.ParseInt(sb.String(), 2, 64)
-	return
+	number, err := strconv.ParseInt(sb.String(), 2, 64)
+	shared.Handle(err)
+	return int(number)
 }
 
-func readBinaryStrings(r io.Reader) (bins []bitSlice, err error) {
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		line := scanner.Text()
+func parseBinaryStrings(lines []string) (bins []bitSlice) {
+	for _, line := range lines {
 		bin := make(bitSlice, len(line))
-		for i, char := range line {
-			bin[i], err = strconv.Atoi(string(char))
-			if err != nil || !(bin[i] == 0 || bin[i] == 1) {
-				return
-			}
+		for i, r := range line {
+			number, err := strconv.Atoi(string(r))
+			shared.Handle(err)
+			bin[i] = number
 		}
 		bins = append(bins, bin)
 	}
@@ -44,19 +33,16 @@ func readBinaryStrings(r io.Reader) (bins []bitSlice, err error) {
 }
 
 func extractRates(bins []bitSlice) (mostCommon, leastCommon bitSlice) {
+	mostCommon, leastCommon = make(bitSlice, len(bins[0])), make(bitSlice, len(bins[0]))
 	binSum := make(bitSlice, len(bins[0]))
-	mostCommon = make(bitSlice, len(bins[0]))
-	leastCommon = make(bitSlice, len(bins[0]))
 	for i := range binSum {
 		for _, bin := range bins {
 			binSum[i] += bin[i]
 		}
 		if binSum[i] >= len(bins)/2 {
-			mostCommon[i] = 1
-			leastCommon[i] = 0
+			mostCommon[i], leastCommon[i] = 1, 0
 		} else {
-			mostCommon[i] = 0
-			leastCommon[i] = 1
+			mostCommon[i], leastCommon[i] = 0, 1
 		}
 	}
 	return
@@ -80,7 +66,6 @@ func leastCommonBit(bins []bitSlice, position int) int {
 
 func binarySieve(bins []bitSlice, targetBit func([]bitSlice, int) int) bitSlice {
 	validBins := append([]bitSlice(nil), bins...)
-	fmt.Printf("Valid Binaries: %d out of %d total, ratio: %f\n", len(validBins), len(bins), float64(len(validBins))/float64(len(bins)))
 	for bitIndex := range bins[0] {
 		var remainingValid []bitSlice
 		target := targetBit(validBins, bitIndex)
@@ -89,7 +74,6 @@ func binarySieve(bins []bitSlice, targetBit func([]bitSlice, int) int) bitSlice 
 				remainingValid = append(remainingValid, bin)
 			}
 		}
-		fmt.Printf("Valid Binaries: %d out of %d total, ratio: %f\n", len(remainingValid), len(validBins), float64(len(remainingValid))/float64(len(validBins)))
 		if len(remainingValid) == 1 {
 			return remainingValid[0]
 		}
@@ -99,33 +83,18 @@ func binarySieve(bins []bitSlice, targetBit func([]bitSlice, int) int) bitSlice 
 }
 
 func main() {
-	inputFilePath := "diagnostic_report.txt"
-	reader, err := os.Open(inputFilePath)
-	handle(err)
-	defer reader.Close()
-
-	bins, err := readBinaryStrings(reader)
-	handle(err)
+	lines := shared.ParseInputFile("input.txt")
+	bins := parseBinaryStrings(lines)
 
 	mostCommonBits, leastCommonBits := extractRates(bins)
-	fmt.Println(mostCommonBits)
-	fmt.Println(leastCommonBits)
-	epsilon, err := bitSliceToInt(mostCommonBits)
-	handle(err)
-	gamma, err := bitSliceToInt(leastCommonBits)
-	handle(err)
+	epsilon, gamma := bitSliceToInt(mostCommonBits), bitSliceToInt(leastCommonBits)
 	powerConsumption := epsilon * gamma
-	fmt.Printf("ɛ = %d, γ = %d → Power consumption is %d.", epsilon, gamma, powerConsumption)
+	fmt.Printf("ɛ = %d, γ = %d → Power consumption is %d.\n",
+		epsilon, gamma, powerConsumption)
 
-	o2Bits := binarySieve(bins, mostCommonBit)
-	co2Bits := binarySieve(bins, leastCommonBit)
-	fmt.Println(o2Bits)
-	fmt.Println(co2Bits)
-	o2Rating, err := bitSliceToInt(o2Bits)
-	handle(err)
-	co2Rating, err := bitSliceToInt(co2Bits)
-	handle(err)
+	o2Bits, co2Bits := binarySieve(bins, mostCommonBit), binarySieve(bins, leastCommonBit)
+	o2Rating, co2Rating := bitSliceToInt(o2Bits), bitSliceToInt(co2Bits)
 	lifeSupportRating := o2Rating * co2Rating
-	fmt.Printf("O2 Generation = %d, CO2 Scrubbing = %d → Life Support Rating is %d.",
+	fmt.Printf("O2 Generation = %d, CO2 Scrubbing = %d → Life Support Rating is %d.\n",
 		o2Rating, co2Rating, lifeSupportRating)
 }
