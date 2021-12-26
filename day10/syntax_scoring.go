7bbe188b4e2e2e18761a93ab8d711bf796911846
Anton Eder <anton.eder@tngtech.com>
2021-12-26 23:49:50 +0100

    streamline day10

--- day10/syntax_scoring.go
+++ day10/syntax_scoring.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
+	"aoc2021/shared"
 	"fmt"
-	"log"
-	"os"
 	"sort"
 )
 
@@ -29,6 +27,17 @@ var Closing = map[rune]rune{
 	'<': '>',
 }
 
+type Stack []rune
+
+func (s Stack) Push(r rune) Stack {
+	return append(s, r)
+}
+
+func (s Stack) Pop() (rune, Stack) {
+	final := len(s) - 1
+	return s[final], s[:final]
+}
+
 func isOpening(r rune) bool {
 	for bracket := range Closing {
 		if r == bracket {
@@ -47,54 +56,20 @@ func isClosing(r rune) bool {
 	return false
 }
 
-type Stack []rune
-
-func (s Stack) Push(r rune) Stack {
-	return append(s, r)
-}
-
-func (s Stack) Pop() (rune, Stack) {
-	final := len(s) - 1
-	return s[final], s[:final]
-}
-
-func main() {
-	inputFile := "lines.txt"
-	lines, err := parseInputFile(inputFile)
-	handle(err)
-
-	scores := make(map[string]sort.IntSlice)
-
-	for _, line := range lines {
-		status, score := validate(line)
-		scores[status] = append(scores[status], score)
-	}
-	fmt.Printf("Corrupted lines score for %d points, incomplete lines for %d points.\n",
-		sum(scores["corrupted"]), middle(scores["incomplete"]))
-}
-
-func handle(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func parseInputFile(filePath string) (lines []string, err error) {
-	reader, err := os.Open(filePath)
-	if err != nil {
-		return
-	}
-	defer reader.Close()
-
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+func sum(numbers sort.IntSlice) (sum int) {
+	for _, n := range numbers {
+		sum += n
 	}
 	return
 }
 
+func middle(numbers sort.IntSlice) int {
+	sort.Sort(numbers)
+	return numbers[len(numbers)/2]
+}
+
 func validate(line string) (status string, score int) {
-	stack := Stack(nil)
+	var stack Stack
 	for _, r := range line {
 		if isOpening(r) {
 			stack = stack.Push(r)
@@ -108,30 +83,35 @@ func validate(line string) (status string, score int) {
 			}
 		}
 	}
+
 	if len(stack) == 0 {
 		status = "valid"
 	} else {
 		status = "incomplete"
-		fmt.Println(string(stack))
-		for len(stack) > 0 {
-			var r rune
-			r, stack = stack.Pop()
-			fmt.Println(score)
-			score *= 5
-			score += incompleteScore[r]
-		}
-		fmt.Println(score)
+		score = scoreIncomplete(stack)
 	}
 	return
 }
 
-func sum(numbers sort.IntSlice) (sum int) {
-	for _, n := range numbers {
-		sum += n
+func scoreIncomplete(stack Stack) (score int) {
+	for len(stack) > 0 {
+		var r rune
+		r, stack = stack.Pop()
+		score *= 5
+		score += incompleteScore[r]
 	}
 	return
 }
-func middle(numbers sort.IntSlice) int {
-	sort.Sort(numbers)
-	return numbers[len(numbers)/2]
+
+func main() {
+	lines := shared.ParseInputFile("input.txt")
+
+	scores := make(map[string]sort.IntSlice)
+	for _, line := range lines {
+		status, score := validate(line)
+		scores[status] = append(scores[status], score)
+	}
+
+	fmt.Printf("Corrupted lines score for %d points, incomplete lines for %d points.\n",
+		sum(scores["corrupted"]), middle(scores["incomplete"]))
 }
