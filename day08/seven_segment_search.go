5f763854334624712d688b28c5af9df7e34776ad
Anton Eder <anton.eder@tngtech.com>
2021-12-26 20:36:47 +0100

    streamline day08

--- day08/seven_segment_search.go
+++ day08/seven_segment_search.go
@@ -1,120 +1,39 @@
 package main
 
 import (
-	"bufio"
-	"errors"
+	"aoc2021/shared"
 	"fmt"
-	"log"
-	"os"
 	"sort"
 	"strconv"
 	"strings"
 )
 
-type Signal []rune
-
-func (s Signal) Contains(target rune) (contains bool) {
-	for _, r := range s {
-		if r == target {
-			contains = true
-			return
-		}
-	}
-	return
+var FrequencyCode = map[Signal]string{
+	"89":      "1",
+	"889":     "7",
+	"6789":    "4",
+	"47788":   "2",
+	"67789":   "5",
+	"77889":   "3",
+	"467789":  "6",
+	"467889":  "0",
+	"677889":  "9",
+	"4677889": "8",
 }
 
-type Code map[string]Signal
-type Cipher map[string]string
-type Wiring map[rune]rune
-type Frequency map[rune]int
+type Signal string
 
-func main() {
-	inputFile := "signals.txt"
-	inputs, outputs, err := parseInputFile(inputFile)
-	handle(err)
+type Display struct{ cipher, code []Signal }
 
-	count := countSimpleOutputs(outputs)
-	fmt.Println(count)
-
-	numbers, err := decipherOutputs(inputs, outputs)
-	handle(err)
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-	}
-	fmt.Println(sum)
+func stringToSignal(s string) Signal {
+	runes := []rune(s)
+	sort.Slice(runes, func(i, j int) bool { return runes[i] < runes[j] })
+	return Signal(runes)
 }
 
-func decipherOutputs(inputs [][]Signal, outputs [][]Signal) (numbers []int, err error) {
-	if len(inputs) != len(outputs) {
-		err = errors.New("input and output have different length")
-		return
-	}
-	for i, in := range inputs {
-		code := solve(in)
-		cipher := invert(code)
-		var sb strings.Builder
-		for _, signal := range outputs[i] {
-			sb.WriteString(cipher[string(signal)])
-		}
-		var number int
-		number, err = strconv.Atoi(sb.String())
-		if err != nil {
-			return
-		}
-		numbers = append(numbers, number)
-	}
-	return
-}
-
-func handle(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func countSimpleOutputs(outputs [][]Signal) int {
-	count := 0
-	for _, out := range outputs {
-		for _, signal := range out {
-			switch len(signal) {
-			case 2, 3, 4, 7:
-				count++
-			}
-		}
-	}
-	return count
-}
-
-func invert(code Code) (cipher Cipher) {
-	cipher = make(Cipher)
-	for digit, signal := range code {
-		cipher[string(signal)] = digit
-	}
-	return
-}
-
-func parseInputFile(filePath string) (inputs [][]Signal, outputs [][]Signal, err error) {
-	reader, err := os.Open(filePath)
-	if err != nil {
-		return
-	}
-	defer reader.Close()
-
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		line := scanner.Text()
-		halves := strings.Split(line, " | ")
-		rawInput, rawOutput := halves[0], halves[1]
-		input, output := stringToSignals(rawInput), stringToSignals(rawOutput)
-		inputs, outputs = append(inputs, input), append(outputs, output)
-	}
-	return
-}
-
-func frequency(signals []Signal) (freq Frequency) {
-	freq = make(Frequency)
-	for _, signal := range signals {
+func (d Display) frequency() (freq map[rune]int) {
+	freq = make(map[rune]int)
+	for _, signal := range d.cipher {
 		for _, r := range signal {
 			freq[r]++
 		}
@@ -122,78 +41,61 @@ func frequency(signals []Signal) (freq Frequency) {
 	return
 }
 
-func solve(input []Signal) (code Code) {
-	code = make(Code)
-	var fivers, sixers []Signal
-	for _, signal := range input {
-		switch len(signal) {
-		case 2:
-			code["1"] = signal
-		case 3:
-			code["7"] = signal
-		case 4:
-			code["4"] = signal
-		case 5:
-			fivers = append(fivers, signal)
-		case 6:
-			sixers = append(sixers, signal)
-		case 7:
-			code["8"] = signal
-		}
-	}
-	wire := make(Wiring)
-	for r, freq := range frequency(input) {
-		switch freq {
-		case 4:
-			wire['e'] = r
-		case 6:
-			wire['b'] = r
-		case 8:
-			if code["1"].Contains(r) {
-				wire['c'] = r
-			}
-		case 9:
-			wire['f'] = r
-		}
-	}
-	var remainingFivers []Signal
-	for _, signal := range fivers {
-		if !signal.Contains(wire['f']) {
-			code["2"] = signal
-		} else {
-			remainingFivers = append(remainingFivers, signal)
-		}
-	}
-	for _, signal := range remainingFivers {
-		if signal.Contains(wire['b']) {
-			code["5"] = signal
-		} else {
-			code["3"] = signal
-		}
-	}
-	var remainingSixers []Signal
-	for _, signal := range sixers {
-		if !signal.Contains(wire['e']) {
-			code["9"] = signal
-		} else {
-			remainingSixers = append(remainingSixers, signal)
-		}
-	}
-	for _, signal := range remainingSixers {
-		if signal.Contains(wire['c']) {
-			code["0"] = signal
-		} else {
-			code["6"] = signal
+func (d Display) countSimpleSignals() (count int) {
+	for _, s := range d.code {
+		switch len(s) {
+		case 2, 3, 4, 7:
+			count++
 		}
 	}
 	return
 }
 
-func stringToSignals(rawInput string) (signals []Signal) {
-	for _, signalString := range strings.Split(rawInput, " ") {
-		signal := Signal(signalString)
-		sort.Slice(signal, func(i, j int) bool { return signal[i] < signal[j] })
+func (d Display) decode() (digits string) {
+	frequency := d.frequency()
+	for _, signal := range d.code {
+		id := identify(frequency, signal)
+		digit := FrequencyCode[id]
+		digits += digit
+	}
+	return
+}
+
+func identify(frequency map[rune]int, s Signal) (id Signal) {
+	var frequencies string
+	for _, r := range s {
+		frequencies += strconv.Itoa(frequency[r])
+	}
+	return stringToSignal(frequencies)
+}
+
+func parseDisplays(lines []string) (displays []Display) {
+	for _, line := range lines {
+		halves := strings.Split(line, " | ")
+		cipher, code := parseSignals(halves[0]), parseSignals(halves[1])
+		displays = append(displays, Display{cipher, code})
+	}
+	return
+}
+
+func parseSignals(line string) (signals []Signal) {
+	for _, s := range strings.Split(line, " ") {
+		signal := stringToSignal(s)
 		signals = append(signals, signal)
 	}
 	return
+}
+
+func main() {
+	lines := shared.ParseInputFile("input.txt")
+	displays := parseDisplays(lines)
+
+	sum, simpleSignals := 0, 0
+	for _, display := range displays {
+		simpleSignals += display.countSimpleSignals()
+		number, _ := strconv.Atoi(display.decode())
+		sum += number
+	}
+	fmt.Printf("%d digits could be decoded effortlessly.\n", simpleSignals)
+	fmt.Printf("The sum of all digits is %d.\n", sum)
 }
